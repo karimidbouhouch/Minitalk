0addfb1e4739b239633dbc71274d5af1f40fcd16
karimidbouhouch <karim.idbouhouch@gmail.com>
2021-12-17 01:39:07 +0100

    adding header bonus

---
+++ minitalk_bonus.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk_bonus.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kid-bouh <kid-bouh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/16 22:56:49 by kid-bouh          #+#    #+#             */
+/*   Updated: 2021/12/16 22:56:58 by kid-bouh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <signal.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+void	ft_putstr(char *str);
+int		ft_atoi(const char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+int		ft_strlen(const char *s);
+char	*ft_strdup(const char *s);
+void	ft_putnbr(int n);
+void	ft_putchar(char c);
+
+#endif
